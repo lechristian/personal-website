7e68c02021a53aee081fd1e76cebfadb2400f60d
Christian Le <christianle94@gmail.com>
2015-12-28 18:53:12 -0800

    split webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,188 +6,33 @@
 
 'use strict';
 
+const _ = require('lodash');
 const path = require('path');
 const webpack = require('webpack');
 const cssnano = require('cssnano');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const WebpackOutputStatsPlugin = require('./utils/webpack-output-stats-plugin');
+const logColors = require('./config/colors');
 
-const nodeEnvironment = process.env.NODE_ENV;
+const isDev = process.env.NODE_ENV === 'development';
 
-let logColors;
-if (nodeEnvironment !== 'production') {
-  logColors = require('./config/colors');
-}
+let webpackConfig = require('./webpack.config.base');
+const webpackAdd = isDev
+  ? require('./webpack.config.dev')
+  : require('./webpack.config.prod');
 
-Object.assign = Object.assign || require('object-assign');
+webpackConfig.devtool = webpackAdd.devtool;
+webpackConfig.entry = webpackAdd.entry;
+webpackConfig.plugins = webpackConfig.plugins.concat(webpackAdd.plugins);
 
-let webpackConfig = {
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  resolve: {
-    root: [
-      path.resolve(__dirname),
-      path.resolve(__dirname, 'node_modules')
-    ],
-    alias: {
-      imgsRoot: path.resolve(__dirname, 'static/images')
-    }
-  },
-  postcss: [
-    cssnano({
-      sourcemap: true,
-      autoprefixer: {
-        add: true,
-        remove: true,
-        browsers: [
-          'last 3 versions',
-          'ie >= 8',
-          '> 2%'
-        ]
-      },
-      discardComments: {
-        removeAll: true
-      }
-    })
-  ]
-};
+_.forEach(webpackAdd.module, function(value, key) {
+  let temp = webpackConfig.module[key] || [];
+  temp = temp.concat(value);
 
-if (nodeEnvironment === 'production' || nodeEnvironment === 'staging') {
-  webpackConfig = Object.assign(webpackConfig, {
-    devtool: 'source-map',
-    entry: [
-      './src/client/index.js'
-    ],
-    module: {
-      loaders: [
-        {
-          test: /\.(js|jsx)$/,
-          loaders: ['babel'],
-          exclude: /node_modules/,
-          include: __dirname
-        },
-        {
-          test: /\.(jpe?g|png|gif|svg)$/i,
-          loaders: [
-            'file?hash=sha512&digest=hex&name=[hash].[ext]',
-            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-          ]
-        },
-        {
-          test: /\.json$/,
-          loader: 'json'
-        },
-        {
-          test: /\.scss$/,
-          loader: ExtractTextPlugin.extract(
-            'style-loader',
-            'css-loader?minimize!postcss!sass-loader'
-          )
-        }
-      ]
-    },
-    plugins: [
-      new webpack.NoErrorsPlugin(),
-      new webpack.optimize.OccurenceOrderPlugin(),
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('production'),
-          BROWSER: JSON.stringify(true)
-        }
-      }),
-      new ExtractTextPlugin('style.css'),
-      new webpack.optimize.UglifyJsPlugin({
-        minimize: true,
-        screw_ie8: false
-      }),
-      new WebpackOutputStatsPlugin()
-    ]
-  });
-} else {
-  webpackConfig = Object.assign(webpackConfig, {
-    devtool: 'cheap-module-eval-source-map',
-    entry: [
-      'webpack-hot-middleware/client',
-      './src/client/index.js'
-    ],
-    module: {
-      preLoaders: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: [
-            /dist/,
-            /node_modules/
-          ],
-          loaders: [
-            'eslint'
-          ]
-        }
-      ],
-      loaders: [
-        {
-          test: /\.(js|jsx)$/,
-          loader: 'babel',
-          exclude: /node_modules/,
-          include: __dirname,
-          query: {
-            env: {
-              development: {
-                plugins: [
-                  [
-                    'react-transform',
-                    {
-                      transforms: [
-                        {
-                          transform: 'react-transform-catch-errors',
-                          imports: [
-                            'react',
-                            'redbox-react'
-                          ]
-                        }
-                      ]
-                    }
-                  ]
-                ]
-              }
-            }
-          }
-        },
-        {
-          test: /\.json$/,
-          loader: 'json'
-        },
-        {
-          test: /\.(jpe?g|png|gif|svg)$/i,
-          loaders: [
-            'file?hash=sha512&digest=hex&name=[hash].[ext]',
-            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-          ]
-        },
-        {
-          test: /\.scss$/,
-          loaders: [
-            'style',
-            'css?sourceMap',
-            'postcss',
-            'sass'
-          ]
-        }
-      ]
-    },
-    plugins: [
-      new webpack.NoErrorsPlugin(),
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.DefinePlugin({
-        'process.env': {
-          BROWSER: JSON.stringify(true)
-        }
-      })
-    ]
-  });
+  webpackConfig.module[key] = temp;
+});
 
+if (isDev) {
   const loaders = webpackConfig.module.loaders.map(function (loader) {
     if (/babel/.test(loader.loader)) {
       console.log(logColors.cWarning('Applying HMR'));
---
+++ webpack.config.prod.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const WebpackOutputStatsPlugin = require('./utils/webpack-output-stats-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    './src/client/index.js'
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loaders: ['babel'],
+        exclude: /node_modules/,
+        include: __dirname
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(
+          'style-loader',
+          'css-loader?minimize!postcss!sass-loader'
+        )
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new ExtractTextPlugin('style.css'),
+    new WebpackOutputStatsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+        BROWSER: JSON.stringify(true)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      screw_ie8: false
+    })
+  ]
+};
