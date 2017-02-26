1f478b7f465df6b630dd74c64ebaac53543a9d2b
Christian Le <christianle94@gmail.com>
2017-02-26 15:33:59 -0800

    Nuking the repo and starting new

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 /* ==========================================================================
  * ./index.js
  *
- * Path and Babel initialization
+ * App Entry Point
  * ========================================================================== */
 
-require('app-module-path/register');
-
-var fs = require('fs-extra-promise');
-var babelrc = fs.readFileSync('./.babelrc');
-var config;
-try {
-  config = JSON.parse(babelrc);
-} catch(err) {
-  console.log('==> ERROR: Error parsing your .babelrc.');
-  console.log(err.toString);
-}
-
-require('babel-core/register')(config);
-require('./utils/generateBlurbsSummary')(function() {
-  require('./src/server');
-});
