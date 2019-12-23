ff33e4972f1e22edf7b7a7500a0291adccabba31
Christian Le <hi@christianle.com>
2019-12-23 02:15:45 -0800

    Basic terminal and closing interaction

--- components/shell.tsx
+++ components/shell.tsx
@@ -1,79 +1,188 @@
-import React from 'react'
+import React, { Component } from 'react';
+import update from 'immutability-helper';
 
-const Shell = () => (
-  <div className="shell">
-    <div className="window">
-      <div className="close">
-        &#215;
+type ShellProps = {
+  isOpen: boolean,
+  toggleTerminal: (isOpen: boolean) => void
+}
+
+type ShellState = {
+  isTerminalOpened: boolean,
+}
+
+class Shell extends Component<ShellProps, ShellState> {
+
+  componentDidMount() {
+    this.props.toggleTerminal(true);
+  }
+
+  openTerminal() {
+    this.props.toggleTerminal(true);
+  }
+
+  closeTerminal() {
+    this.props.toggleTerminal(false);
+  }
+
+  toggleTerminal() {
+    if (this.props.isOpen) {
+      this.closeTerminal();
+    } else {
+      this.openTerminal();
+    }
+  }
+
+  shellClassName() {
+    let className = 'shell';
+    if (!this.props.isOpen) {
+      className += ' shell-closed';
+    }
+    return className;
+  }
+
+  dockClassName() {
+    let className = 'dock';
+    if (this.props.isOpen) {
+      className += ' dock-hidden';
+    }
+    return className;
+  }
+
+  render() {
+    return (
+      <div className="terminal">
+        <div className={this.shellClassName()}>
+          <div className="window">
+            <div className="close" onClick={this.toggleTerminal.bind(this)}>
+              &#215;
+            </div>
+            <div className="windowTitle">
+              cle
+            </div>
+          </div>
+        </div>
+
+        <div className={this.dockClassName()} onClick={this.toggleTerminal.bind(this)}>
+          <div className="icon">
+            <div className="bar"></div>
+            <div className="arrow">
+              $ &#62;
+            </div>
+          </div>
+        </div>
+
+        <style jsx>{`
+          @media (max-width: 1080px) {
+            .terminal {
+              display: none;
+            }
+          }
+
+          .shell {
+            position: fixed;
+            background: #0d1622;
+            top: 24px;
+            left: 24px;
+            bottom: 24px;
+            right: 24px;
+            border-radius: 8px;
+            overflow: hidden;
+            transition: 0.25s;
+          }
+
+          .shell-closed {
+            top: 100%;
+            bottom: 0;
+            left: 50%;
+            right: 50%;
+          }
+
+          .window {
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 36px;
+            width: 100%;
+            background: #6d737a;
+          }
+
+          .windowTitle {
+            text-align: center;
+            width: 100%;
+            line-height: 36px;
+          }
+
+          .close {
+            position: absolute;
+            top: 8px;
+            left: 8px;
+            width: 20px;
+            height: 20px;
+            border-radius: 10px;
+            overflow: hidden;
+            background: #e94057;
+            text-align: center;
+            color: rgba(0, 0, 0, 0);
+            font-weight: bold;
+            font-size: 20px;
+            line-height: 20px;
+            transition: 0.2s;
+            cursor: pointer;
+          }
+
+          .close:hover {
+            color: #101010;
+          }
+
+          .input {
+            position: absolute;
+            top: 36px;
+            right: 0;
+            left: 0;
+            bottom: 0;
+            padding: 16px;
+          }
+
+          .dock {
+            position: fixed;
+            bottom: 0px;
+            left: 0;
+            right: 0;
+            height: 64px;
+            cursor: pointer;
+            transition: 0.2s;
+          }
+
+          .dock-hidden {
+            bottom: -72px;
+          }
+
+          .icon {
+            margin: 0 auto;
+            height: 48px;
+            width: 48px;
+            overflow: hidden;
+            background: #0d1622;
+            border-radius: 4px;
+          }
+
+          .bar {
+            height: 8px;
+            width: 100%;
+            background: #6d737a;
+          }
+
+          .arrow {
+            color: #6d737a;
+            font-size: 18px;
+            font-weight: bold;
+            padding: 4px;
+          }
+        `}</style>
       </div>
-      <div className="windowTitle">
-        cle
-      </div>
-    </div>
-
-    <div className="input">
-
-    </div>
-
-    <style jsx>{`
-      .shell {
-        position: fixed;
-        background: #0d1622;
-        top: 24px;
-        left: 24px;
-        bottom: 24px;
-        right: 24px;
-        border-radius: 8px;
-        overflow: hidden;
-      }
-
-      .window {
-        position: absolute;
-        top: 0;
-        left: 0;
-        height: 36px;
-        width: 100%;
-        background: #6d737a;
-      }
-
-      .windowTitle {
-        text-align: center;
-        width: 100%;
-        line-height: 36px;
-      }
-
-      .input {
-        position: absolute;
-        top: 36px;
-        right: 0;
-        left: 0;
-        bottom: 0;
-        padding: 16px;
-      }
-
-      .close {
-        position: absolute;
-        top: 8px;
-        left: 8px;
-        width: 20px;
-        height: 20px;
-        border-radius: 10px;
-        overflow: hidden;
-        background: #e94057;
-        text-align: center;
-        color: rgba(0, 0, 0, 0);
-        font-weight: bold;
-        font-size: 20px;
-        line-height: 20px;
-        transition: 0.2s;
-        cursor: pointer;
-      }
-
-      .close:hover {
-        color: #101010;
-      }
-    `}</style>
-  </div>
-);
+    );
+  }
+}
 
 export default Shell;
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,162 +1,230 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Head from 'next/head'
 import Shell from '../components/shell'
+import update from 'immutability-helper';
 
-const Home = () => (
-  <div>
-    <Head>
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+type HomeState = {
+  isTerminalEnabled: boolean,
+  isTerminalOpened: boolean,
+}
 
-      <title>Christian Le | cle</title>
+class Home extends Component<{}, HomeState> {
 
-      <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-      <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
-      <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
-      <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-      <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-      <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
-      <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
-      <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-      <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="manifest" href="/manifest.json" />
-      <meta name="msapplication-TileColor" content="#e6e7e8" />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-      <meta name="theme-color" content="#e6e7e8" />
+  state = {
+    isTerminalEnabled: false,
+    isTerminalOpened: false,
+  };
 
-      <link href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css' rel='stylesheet' type='text/css' />
-      <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,700' rel='stylesheet' type='text/css' />
-      <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,300italic,400italic,600,600italic' rel='stylesheet' type='text/css' />
-      <link href='https://fonts.googleapis.com/css?family=Source+Code+Pro:400,300,500,700' rel='stylesheet' type='text/css' />
-    </Head>
+  componentDidMount() {
+    this.setState({
+      isTerminalEnabled: true,
+    });
+  }
 
-    <div className="home">
-      <div className="logo">
-        <img src="/cle.png" />
-      </div>
-      <div className="info">
-        <p>
-          <div className="h2">
-            Hi there! I'm <b>Christian Le</b>,
+  updateTerminalState(isOpen: boolean) {
+    this.setState({
+      isTerminalOpened: isOpen,
+    });
+  }
+
+  homeClassName() {
+    let className = 'home';
+    if (!this.state.isTerminalEnabled || !this.state.isTerminalOpened) {
+      return className;
+    }
+    return `${className} home-hidden`;
+  }
+
+  render() {
+    return (
+      <div className="cle">
+        <Head>
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+          <title>Christian Le | cle</title>
+
+          <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+          <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="msapplication-TileColor" content="#e6e7e8" />
+          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+          <meta name="theme-color" content="#e6e7e8" />
+
+          <link href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css' rel='stylesheet' type='text/css' />
+          <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,700' rel='stylesheet' type='text/css' />
+          <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,300italic,400italic,600,600italic' rel='stylesheet' type='text/css' />
+          <link href='https://fonts.googleapis.com/css?family=Source+Code+Pro:400,300,500,700' rel='stylesheet' type='text/css' />
+        </Head>
+
+        <div className={this.homeClassName()}>
+          <div className="logo">
+            <img src="/cle.png" />
           </div>
-        </p>
-        <p>
-          <div className="h3">
-            a <b>
-              <a href="https://github.com/lechristian" target="_blank">
-                software engineer
-              </a>
-            </b> born and raised in <b>
-              <a href="https://goo.gl/Jpbpks" target="_blank">
-                Southern California
-              </a>
-            </b> currently residing in the <b>
-              <a href="https://goo.gl/knXZm4" target="_blank">
-                Bay Area
-              </a>
-            </b>. I graduated from <b>
-              <a href="http://www.berkeley.edu/" target="_blank">
-                UC Berkeley
-              </a>
-            </b>, co-founded <b>Outcomes.com</b>, helped grow <b>
-              <a href="https://yelp.com/reservations" target="_blank">
-                Yelp Reservations
-              </a>
-            </b>, and now work on the <b>
-              <a href="https://squareup.com/business-debit-card" target="_blank">
-                Square Card
-              </a>
-            </b>.
+          <div className="info">
+            <p className="h2">
+              Hi there! I'm <b>Christian Le</b>,
+            </p>
+            <p className="h3">
+              a <b>
+                <a href="https://github.com/lechristian" target="_blank">
+                  software engineer
+                </a>
+              </b> born and raised in <b>
+                <a href="https://goo.gl/Jpbpks" target="_blank">
+                  Southern California
+                </a>
+              </b> currently residing in the <b>
+                <a href="https://goo.gl/knXZm4" target="_blank">
+                  Bay Area
+                </a>
+              </b>. I graduated from <b>
+                <a href="http://www.berkeley.edu/" target="_blank">
+                  UC Berkeley
+                </a>
+              </b>, co-founded <b>Outcomes.com</b>, helped grow <b>
+                <a href="https://yelp.com/reservations" target="_blank">
+                  Yelp Reservations
+                </a>
+              </b>, and now work on the <b>
+                <a href="https://squareup.com/business-debit-card" target="_blank">
+                  Square Card
+                </a>
+              </b>.
+            </p>
           </div>
-        </p>
-      </div>
-      <div className="photos">
-        <p>
-          You can find a small collection of my photos at <b>
-            <a href="https://thecle.me" target="_blank">
-              thecle.me
-            </a>
-          </b>.
-        </p>
-      </div>
-      <div className="footer">
-        <div className="link">
-          <a href="https://github.com/lechristian" target="_blank">GitHub</a>
+          <div className="photos">
+            <p>
+              You can find a small collection of my photos at <b>
+                <a href="https://thecle.me" target="_blank">
+                  thecle.me
+                </a>
+              </b>.
+            </p>
+          </div>
+          <div className="footer">
+            <div className="link">
+              <a href="https://github.com/lechristian" target="_blank">GitHub</a>
+            </div>
+            <div className="link">
+              <a href="https://www.linkedin.com/in/christianle94" target="_blank">LinkedIn</a>
+            </div>
+          </div>
         </div>
-        <div className="link">
-          <a href="https://www.linkedin.com/in/christianle94" target="_blank">LinkedIn</a>
-        </div>
+
+        { this.state.isTerminalEnabled ? (
+          <Shell
+            isOpen={this.state.isTerminalOpened}
+            toggleTerminal={this.updateTerminalState.bind(this)}
+          />
+        ) : (<div className="empty"></div>) }
+
+        <style jsx global>{`
+          html, body {
+            background-color: #e6e7e8;
+            color: #1f1f1f;
+            font-family: "Source Sans Pro", Roboto, Helvetica, Arial, sans-serif;
+            line-height: 1.25;
+          }
+          a {
+            text-decoration: none;
+            color: #1f1f1f;
+          }
+          a:hover {
+            color: #55b4d6;
+          }
+        `}</style>
+
+        <style jsx>{`
+          .cle {
+            height: 100%;
+          }
+
+          .empty {
+            display: none;
+            height: 0;
+            width: 0;
+            position: fixed;
+            top: 0;
+            left: 0;
+          }
+
+          .home {
+            height: 100%;
+            max-width: 720px;
+            padding: 5%;
+            margin: 0 auto;
+            opacity: 1;
+            overflow: scroll;
+            transition: 0.2s;
+          }
+
+          .home-hidden {
+            opacity: 0;
+            overflow: hidden;
+          }
+
+          @media (max-width: 1080px) {
+            .home-hidden {
+              opacity: 1;
+              overflow: scroll;
+            }
+          }
+
+          .h2 {
+            font-size: 32px;
+          }
+
+          .h3 {
+            font-size: 24px;
+            font-weight: 300;
+          }
+
+          .logo {
+            text-align: center;
+            max-width: 240px;
+            margin: 0 auto;
+          }
+
+          .logo img {
+            width: 100%;
+          }
+
+          .info {
+            margin-top: 2.5rem;
+            text-align: justify;
+          }
+
+          .photos {
+            font-size: 18px;
+            font-weight: 300;
+          }
+
+          .footer {
+            text-align: center;
+            margin-top: 2.5rem;
+          }
+          .footer .link {
+            display: inline-block;
+            margin: 0 12px;
+          }
+        `}</style>
       </div>
+    );
+  }
 
-      <Shell />
-    </div>
+}
 
-    <style jsx global>{`
-      html, body {
-        background-color: #e6e7e8;
-        color: #1f1f1f;
-        font-family: "Source Sans Pro", Roboto, Helvetica, Arial, sans-serif;
-        line-height: 1.25;
-      }
-      a {
-        text-decoration: none;
-        color: #1f1f1f;
-      }
-      a:hover {
-        color: #55b4d6;
-      }
-    `}</style>
-
-    <style jsx>{`
-      .home {
-        max-width: 720px;
-        padding: 5%;
-        margin: 0 auto;
-      }
-
-      .h2 {
-        font-size: 32px;
-      }
-
-      .h3 {
-        font-size: 24px;
-        font-weight: 300;
-      }
-
-      .logo {
-        text-align: center;
-        max-width: 240px;
-        margin: 0 auto;
-      }
-
-      .logo img {
-        width: 100%;
-      }
-
-      .info {
-        margin-top: 2.5rem;
-        text-align: justify;
-      }
-
-      .photos {
-        font-size: 18px;
-        font-weight: 300;
-      }
-
-      .footer {
-        text-align: center;
-        margin-top: 2.5rem;
-      }
-      .footer .link {
-        display: inline-block;
-        margin: 0 12px;
-      }
-    `}</style>
-  </div>
-)
 
 export default Home
